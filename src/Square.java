d575e5c507d131a1e0e355038787cda3006976f7
Nicholas Ellul <nicholas.ellul1@gmail.com>
2016-11-17 12:48:10 -0500

    Small syntax fix

--- src/Square.java
+++ src/Square.java
@@ -11,13 +11,17 @@ public class Square {
 		orange ("o"),
 		blue ("b"),
 		white ("w"),
-		yellow ("y")
+		yellow ("y");
 		
 	    private final String name;       
 
-	    private COLOR (String s) {
-	        name = s;
+	    private COLOR (String charCode) {
+	        name = charCode;
 	    }
+
+		public String getName() {
+			return name;
+		}
 
 	};
 	
@@ -26,34 +30,4 @@ public class Square {
 		this._colorOfFace = colorOfFace;
 	}
 	
-	
-	
-	
-	/*public enum POSITION
-	{
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-		13,
-		14,
-		15,
-		16,
-		17,
-		18,
-		19,
-		20,
-		21,
-		22,
-		23,
-		24
-	};*/
 }
